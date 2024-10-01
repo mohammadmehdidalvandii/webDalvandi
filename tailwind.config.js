0633aaa90f857b0d3bb25b6c77e7dc9198575954
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-01 16:36:36 +0330

    project strat add a file and package

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,46 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily:{
+        "iranMedium":"iranMedium",
+        "iranBold" : "iranBold",
+        "iranBlack": "iranBlack",
       },
+      fontSize:{
+        "md":"2rem"
+      },
+      colors: {
+        "background":{
+          100: "#FFFFFF",
+          200:"#F3F4F6"
+        },
+        "font":{
+          100:"#3D434F ",
+          200:"#AAB0BB ",
+          300:"#585D67",
+        },
+        "primary":{
+          default:"#0EA4E8 ",
+          100:"#0284C7 "
+        },
+        "secondary":{
+          default:"#38D277",
+          100:"#16A34A"
+        }
+      },
+      borderRadius:{
+        50:"50%"
+      },
+      container:{
+        center:"true"
+      },
+      screens:{
+        "xs":"380px",
+        "sm": "576px",
+        "md":"768px",
+        "lg":"992px",
+        "xl":"1220px"
+      }
     },
   },
   plugins: [],
