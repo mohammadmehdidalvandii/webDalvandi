7d54dfc60c21678495c62394fab3c1d0e2be0251
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-03 14:33:40 +0330

    created page AboutMe

---
+++ src/app/AboutMe/page.jsx
@@ -0,0 +1,17 @@
+import Navbar from '@/components/module/Navbar/Navbar';
+import React from 'react'
+
+export const metadata = {
+    title: "  درباره من |  دالوندی",
+    description: "برنامه نویس فرانت اند ",
+  };
+
+function page() {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default page
