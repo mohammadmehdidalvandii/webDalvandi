5b08808b2672d3b843666fad5fc14cdb01d3cfb5
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-05 14:41:06 +0330

    created projects page

---
+++ src/app/Projects/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/module/Navbar/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  )
+}
+
+export default page
