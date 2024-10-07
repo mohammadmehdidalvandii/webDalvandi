8598a35eca7dbed89d54518b3f9f63ca890bea25
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-07 15:17:00 +0330

    created section Blog for Articles page

--- src/app/Articles/page.jsx
+++ src/app/Articles/page.jsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/module/Navbar/Navbar';
+import Blog from '@/components/template/articles/Blog/Blog';
 import React from 'react';
 
 function page() {
   return (
     <>
     <Navbar/>
+    <Blog/>
     </>
   )
 }
---
+++ src/components/template/articles/Blog/Blog.jsx
@@ -0,0 +1,31 @@
+import ArticleCart from "@/components/module/ArticleCart/ArticleCart";
+import React from "react";
+
+function Blog() {
+  return (
+    <section className="block mt-12">
+      <div className="container">
+        <div className="block">
+          <h6 className="block text-center font-iranBlack text-3xl text-font-300">
+            مقالات من{" "}
+          </h6>
+        </div>
+        {/* <div className="block mt-12">
+          <h6 className="block text-center font-iranBlack text-3xl text-secondary-default">
+            تا الان مقاله ای منتشر نشد{" "}
+          </h6>
+        </div> */}
+
+        <div className="grid mt-12 gap-6 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+            <ArticleCart/>
+            <ArticleCart/>
+            <ArticleCart/>
+            <ArticleCart/>
+            <ArticleCart/>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Blog;
