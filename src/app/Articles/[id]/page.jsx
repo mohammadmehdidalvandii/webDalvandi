89e5b211fc5f05247df1c03d44f4c7fba4f5f920
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-07 16:00:06 +0330

    created section ArticleContent ArticleSingle

--- src/app/Articles/[id]/page.jsx
+++ src/app/Articles/[id]/page.jsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/module/Navbar/Navbar'
+import ArticleContent from '@/components/template/articleSingle/ArticleContent/ArticleContent'
 import React from 'react'
 
 function page() {
   return (
     <>
     <Navbar/>
+    <ArticleContent/>
     </>
   )
 }
