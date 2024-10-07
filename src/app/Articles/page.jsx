ac610ae3b68e898763e84a1b017c990bf1c0f59b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-07 15:28:05 +0330

    add metaData for article and project

--- src/app/Articles/page.jsx
+++ src/app/Articles/page.jsx
@@ -2,6 +2,11 @@ import Navbar from '@/components/module/Navbar/Navbar';
 import Blog from '@/components/template/articles/Blog/Blog';
 import React from 'react';
 
+export const metadata = {
+    title: "   مقالات |  دالوندی",
+    description: "برنامه نویس فرانت اند ",
+  };
+
 function page() {
   return (
     <>
--- src/app/Projects/page.jsx
+++ src/app/Projects/page.jsx
@@ -2,6 +2,11 @@ import Navbar from '@/components/module/Navbar/Navbar'
 import Projects from '@/components/template/projects/Projects'
 import React from 'react'
 
+export const metadata = {
+  title: "  پروژه ها |  دالوندی",
+  description: "برنامه نویس فرانت اند ",
+};
+
 function page() {
   return (
     <>
