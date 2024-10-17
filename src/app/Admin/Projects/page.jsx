f98bf48a46690512e0c37c3d3ac4111e47e3ce25
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-17 15:39:38 +0330

    created section myProjects

--- src/app/Admin/Projects/page.jsx
+++ src/app/Admin/Projects/page.jsx
@@ -1,10 +1,47 @@
 import AdminLayout from '@/components/layout/AdminLayout/AdminLayout'
+import Link from 'next/link'
 import React from 'react'
 
 function page() {
   return (
     <AdminLayout>
-        this projects
+        <section className="block my-12 h-[600] overscroll-y-auto">
+            <div className="container">
+                <div className="block">
+                    <h4 className="block font-iranBlack text-4xl text-font-300">پروژه های من</h4>
+                </div>
+                <div className="grid mt-12 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                    <div className="block bg-white rounded-lg p-4">
+                        <img src="/assets/images/p-1.png" alt="" className="block" />
+                        <div className="flex justify-between mt-12">
+                            <Link href="" className="btn_primary">مشاهده پروژه</Link>
+                            <button className="btn_red">حذف پروژه</button>
+                        </div>
+                    </div>
+                    <div className="block bg-white rounded-lg p-4">
+                        <img src="/assets/images/p-1.png" alt="" className="block" />
+                        <div className="flex justify-between mt-12">
+                            <Link href="" className="btn_primary">مشاهده پروژه</Link>
+                            <button className="btn_red">حذف پروژه</button>
+                        </div>
+                    </div>
+                    <div className="block bg-white rounded-lg p-4">
+                        <img src="/assets/images/p-1.png" alt="" className="block" />
+                        <div className="flex justify-between mt-12">
+                            <Link href="" className="btn_primary">مشاهده پروژه</Link>
+                            <button className="btn_red">حذف پروژه</button>
+                        </div>
+                    </div>
+                    <div className="block bg-white rounded-lg p-4">
+                        <img src="/assets/images/p-1.png" alt="" className="block" />
+                        <div className="flex justify-between mt-12">
+                            <Link href="" className="btn_primary">مشاهده پروژه</Link>
+                            <button className="btn_red">حذف پروژه</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </AdminLayout>
   )
 }
