b9ccea718240ad38acf023f51e141355ca137ca4
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-22 14:25:07 +0330

    created logic exit user

--- src/components/module/Topbar/Topbar.jsx
+++ src/components/module/Topbar/Topbar.jsx
@@ -1,7 +1,37 @@
+"use client"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function Topbar() {
+
+    const router = useRouter();
+
+    // Logic LogoutHandler
+
+    const handlerLogout = ()=>{
+        swal({
+            title:"آیا از خروج اطمینان دارید ؟",
+            icon:"warning",
+            buttons:["نه","آره"]
+        }).then(async(result)=>{
+            if(result){
+                const res = await fetch('/api/auth/signout',{
+                    method:"POST",
+                });
+                if(res.status === 200){
+                    swal({
+                        title:"با موفقیت از اکانت خارج شدید",
+                        icon:"success",
+                        buttons:"فهمیدم"
+                    }).then(()=>{
+                        router.replace('/')
+                    })
+                }
+            }
+        })
+    }
+
   return (
     <section className="block bg-white h-12 pt-2 px-4">
         <div className="flex items-center justify-between">
@@ -23,7 +53,7 @@ function Topbar() {
                     <Link href='/Admin' className="block font-iranBold text-font-300 duration-300 ease-linear hover:text-secondary-100">تماس با من</Link>
                 </li>
                 <li className="block">
-                    <button className="block font-iranBold text-font-300 duration-300 ease-linear hover:text-red-700">خروج</button>
+                    <button className="block font-iranBold text-font-300 duration-300 ease-linear hover:text-red-700" onClick={handlerLogout}>خروج</button>
                 </li>
             </ul>
         </div>
