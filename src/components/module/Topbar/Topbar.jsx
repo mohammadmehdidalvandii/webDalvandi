2a40a9cbf53a0fb1f7d6d93569e77beb87552e13
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-22 14:36:48 +0330

    created login api is successfully

--- src/components/module/Topbar/Topbar.jsx
+++ src/components/module/Topbar/Topbar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import swal from 'sweetalert'
 
 function Topbar() {
 
--- src/components/template/login/SignIn/SignIn.jsx
+++ src/components/template/login/SignIn/SignIn.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import swal from 'sweetalert'
 
 function SignIn() {
+    const [email , setEmail] =useState()
+const [password , setPassword] =useState()
+
+const router = useRouter()
+
+const handlerIsLogin = async (e)=>{
+    e.preventDefault();
+
+    const user = {email , password};
+
+    const res = await fetch('/api/auth/signin',{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(user),
+    });
+    if(res.status === 200){
+        swal({
+            title:"با موفقیت وارد شدید",
+            icon:"success",
+            buttons:"ورود به پنل کاربری"
+        }).then(()=>{
+            router.replace("/Admin")
+        })
+    }
+}
+
+
   return (
     <section className="block mt-12">
         <div className="container">
@@ -9,13 +39,21 @@ function SignIn() {
                 <form action="#" className="block mt-4">
                     <div className="block w-full mb-6">
                         <labe className="block mr-2 text-sm">نام کاربری یا ایمیل</labe>
-                        <input type="text" className="block w-full h-12 mt-2 border border-font-300 rounded-md outline-secondary-default pr-3" />
+                        <input
+                         type="text"
+                         value={email}
+                         onChange={(e)=>setEmail(e.target.value)}
+                         className="block w-full h-12 mt-2 border border-font-300 rounded-md outline-secondary-default pr-3" />
                     </div>
                     <div className="block w-full ">
                         <labe className="block mr-2 text-sm">رمز عبور</labe>
-                        <input type="text" className="block w-full h-12 mt-2 border border-font-300 rounded-md outline-secondary-default pr-3" />
+                        <input
+                         type="password"
+                         value={password}
+                         onChange={(e)=>setPassword(e.target.value)}
+                         className="block w-full h-12 mt-2 border border-font-300 rounded-md outline-secondary-default pr-3" />
                     </div>
-                    <button className="btn_secondary mt-8 w-full">ورود</button>
+                    <button className="btn_secondary mt-8 w-full" onClick={handlerIsLogin}>ورود</button>
                 </form>
             </div>
         </div>
