548d54661f40ed044b4d5d7abc5b513114c18b56
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-22 15:50:43 +0330

    created connect api contact me

--- src/components/template/contactMe/Contact/Contact.jsx
+++ src/components/template/contactMe/Contact/Contact.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import swal from 'sweetalert';
 
 function Contact() {
+const [username , setUsername] = useState("")
+const [phone , setPhone] = useState("")
+const [email , setEmail] = useState("")
+const [text , setText] = useState("")
+
+const handlerSendMessage = async (e)=>{
+    e.preventDefault();
+
+    console.log("click shod")
+    const message = {username , phone , email ,text};
+
+    const res = await fetch('/api/message',{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(message),
+    });
+
+    if(res.status === 201){
+        swal({
+            title:"پیام شما با موفقیت ارسال شد ",
+            icon:"success",
+            buttons:"متوجه شدم"
+        }).then(()=>{
+            setUsername("")
+            setPhone("")
+            setEmail("")
+            setText("")
+        })
+    }
+}
+
+
+
   return (
     <section className="block mt-12 mb-8">
         <div className="container">
@@ -12,21 +47,41 @@ function Contact() {
                     <form action="" className="block bg-white p-4 rounded-lg">
                         <div className="block mb-3">
                             <label htmlFor="name" className="block pr-2 text-sm font-bold">نام و نام خانوادگی</label>
-                            <input type="text" id="name" className="block w-full h-12 border rounded-md mt-2 outline-secondary-100 pr-2 text-sm font-bold" />
+                            <input
+                             type="text"
+                             value={username}
+                             onChange={(e)=>setUsername(e.target.value)}
+                             id="name"
+                             className="block w-full h-12 border rounded-md mt-2 outline-secondary-100 pr-2 text-sm font-bold" />
                         </div>
                         <div className="block mb-3">
                             <label htmlFor="phone" className="block pr-2 text-sm font-bold">شماره همراه</label>
-                            <input type="text" id="phone" className="block w-full h-12 border rounded-md mt-2 outline-secondary-100 pr-2 text-sm font-bold" />
+                            <input
+                             type="text"
+                             value={phone}
+                             onChange={(e)=>setPhone(e.target.value)}
+                             id="phone"
+                             className="block w-full h-12 border rounded-md mt-2 outline-secondary-100 pr-2 text-sm font-bold" />
                         </div>
                         <div className="block mb-3">
                             <label htmlFor="email" className="block pr-2 text-sm font-bold">ایمیل</label>
-                            <input type="text" id="email" className="block w-full h-12 border rounded-md mt-2 outline-secondary-100 pr-2 text-sm font-bold" />
+                            <input
+                             type="text"
+                             value={email}
+                             onChange={(e)=>setEmail(e.target.value)}
+                             id="email"
+                             className="block w-full h-12 border rounded-md mt-2 outline-secondary-100 pr-2 text-sm font-bold" />
                         </div>
                         <div className="block mb-3">
                             <label htmlFor="message" className="block pr-2 text-sm font-bold">پیام شما</label>
-                            <textarea name="" id="message" className="block w-full h-32 border rounded-md mt-2 outline-secondary-100 pr-2 pt-2 text-sm font-bold"></textarea>
+                            <textarea 
+                            type="text"
+                            value={text}
+                            onChange={(e)=>setText(e.target.value)}
+                             id="message"
+                             className="block w-full h-32 border rounded-md mt-2 outline-secondary-100 pr-2 pt-2 text-sm font-bold"></textarea>
                         </div>
-                        <button className="btn_secondary w-full mt-8">ارسال</button>
+                        <button className="btn_secondary w-full mt-8" onClick={handlerSendMessage}>ارسال</button>
                     </form>
                 </div>
                 <div className="block">
