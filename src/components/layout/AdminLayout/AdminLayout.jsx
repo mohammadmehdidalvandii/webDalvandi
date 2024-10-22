dead5fb42a91644a4eb945277945f75b9fe4d037
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-22 14:17:55 +0330

    add get token user isLogin

--- src/components/layout/AdminLayout/AdminLayout.jsx
+++ src/components/layout/AdminLayout/AdminLayout.jsx
@@ -1,7 +1,29 @@
 import Topbar from '@/components/module/Topbar/Topbar'
+import { authUser } from '@/utils/serverHelpers'
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-function AdminLayout({children}) {
+async function AdminLayout({children}) {
+
+  const user = await authUser();
+
+  // get token User 
+  const token = cookies().get("token");
+  if(!token){
+    return redirect("/Login");
+  }else if(token){
+    if(user.role !== "ADMIN"){
+      swal({
+        title:"شما به این صفحه دسترسی ندارید !",
+        icon:"error",
+        button:"باشه"
+      }).then(()=>{
+        return redirect("/Login")
+      })
+    }
+  }
+
   return (
     <>
     <Topbar/>
