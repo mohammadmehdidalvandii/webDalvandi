4c08adc96ced11691cd37f3c0712f7827c331f09
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-26 17:22:55 +0330

    created logic removed skill admin/about

--- src/components/template/admin/About/About.jsx
+++ src/components/template/admin/About/About.jsx
@@ -1,6 +1,34 @@
+"use client"
 import React from 'react'
 
 function About({skills}) {
+
+    // handler Removed Skill
+
+    const handlerRemovedSkill =(skillID)=>{
+        swal({
+            title:"آیا از حذف تخصص خود اطمینان دارید؟",
+            icon:"warning",
+            buttons:["نه","آره"]
+        }).then(async (result)=>{
+            if(result){
+                const res = await fetch(`/api/skill/${skillID}`,{
+                    method:"DELETE",
+                    headers:{"Content-Type":"application/json"}
+                })
+                if(res.status === 200){
+                    swal({
+                        title:"تخصص مورد نظر حذف شد ",
+                        icon:"success",
+                        buttons:"فهمیدم"
+                    }).then(()=>{
+                        location.reload()
+                    })
+                }
+            }
+        })
+    }
+
   return (
     <section className="block mt-12 mb-8">
         <div className="container">
@@ -11,7 +39,7 @@ function About({skills}) {
                 {skills.map(skill=>(
                          <div className="flex flex-col items-center justify-between bg-white p-4 rounded-lg shadow-md shadow-font-200" key={skill.id}>
                          <img src={skill.img}alt="skill" className="block" />
-                         <button className="btn_red mt-8">حذف</button>
+                         <button className="btn_red mt-8" onClick={()=>handlerRemovedSkill(skill._id)}>حذف</button>
                      </div>
                 ))}
             </div>
