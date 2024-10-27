ea006ce35b1d5fecad676f455dd744b8019f9fa0
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-27 15:14:22 +0330

    fixed style add new tag HomePage

--- src/components/template/home/HomePage/HomePage.jsx
+++ src/components/template/home/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaInstagram  , FaLinkedinIn , FaGithub ,FaTelegram } from "react-icons/fa";
+import { FaInstagram  , FaLinkedinIn , FaGithub ,FaTelegram, FaPhone } from "react-icons/fa";
 
 
 function HomePage() {
@@ -14,23 +14,27 @@ function HomePage() {
                     <p className="block mt-6 text-lg text-font-300">یک مید لول فرانت اند هستم میگن HTML-CSS-JAVASCRIPT ترکیب کنی میشه ازش سایت جالب باحال درست کرد منم درست کردم کنارشم REACT بلدم و ....</p>
                     <div className="flex gap-6 mt-6">
                         <Link href="/AboutMe"  className='btn_secondary'>درباه من</Link>
-                        <Link href="/"  className='btn_primary'>تماس با من</Link>
+                        <Link href="/ContactMe"  className='btn_primary'>تماس با من</Link>
+                        <Link href="" download={true}  className='btn_secondary'>رزومه من</Link>
                     </div>
                     <div className="block mt-8">
                         <h5 className="block font-iranBold text-xl text-font-300">لینک شبکه های اجتماعی</h5>
                         <div className="flex gap-6 mt-4">
-                        <Link href="#" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
+                        <Link href="https://www.instagram.com/dalvandi_dev" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
                             <FaInstagram/>
                         </Link>
-                        <Link href="#" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
+                        <Link href="www.linkedin.com/in/mohammadmehdidalvandii" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
                             <FaLinkedinIn/>
                         </Link>
-                        <Link href="#" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
+                        <Link href="https://github.com/mohammadmehdidalvandii" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
                             <FaGithub/>
                         </Link>
-                        <Link href="#" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
-                            <FaTelegram/>
+                        <Link href="tel:09390944025" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
+                            <FaPhone/>
                         </Link>
+                        {/* <Link href="#" className='text-xl duration-300 ease-linear hover:text-secondary-100' target='_blank'>
+                            <FaTelegram/>
+                        </Link> */}
                         </div>
                     </div>
                 </div>
