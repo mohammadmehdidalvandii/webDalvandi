550f4dcf043cf178eab34f837d6fa5118353def7
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-27 15:06:20 +0330

    fixed navbar

--- src/components/module/Navbar/Navbar.jsx
+++ src/components/module/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <section className="block w-full py-8 bg-background-100">
+    <section className="block w-full py-8 bg-background-100 px-2">
         <div className="container">
             <div className="flex items-center justify-between">
-                <ul className="xs:hidden lg:flex items-center gap-6">
+                <ul className="hidden lg:flex items-center gap-6">
                     <li className="block">
                         <Link href="/" className='link'>خانه</Link>
                     </li>
@@ -20,7 +20,7 @@ function Navbar() {
                         <Link href="/Articles" className='link'>بلاگ</Link>
                     </li>
                 </ul>
-                 <Link href="/" className='block uppercase font-iranBlack text-3xl sm:text-2xl xs:text-lg'><span className="text-secondary-100">dalvnadi</span> - <span className="text-primary-100">front-end</span></Link>
+                 <Link href="/" className='block uppercase font-iranBlack text-sm lg:text-3xl sm:text-2xl xs:text-sm'><span className="text-secondary-100">dalvnadi</span> - <span className="text-primary-100">front-end</span></Link>
 
                  <Link href="/ContactMe" className='link'>تماس با من</Link>
             </div>
