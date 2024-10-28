5d8fe3a8e6639a712a3e325ac86392ee1d05d824
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-28 14:26:30 +0330

    logic Nanbar menu isActive

--- src/components/module/Navbar/Navbar.jsx
+++ src/components/module/Navbar/Navbar.jsx
@@ -1,28 +1,38 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 function Navbar() {
+    const pathName = usePathname();
+    const [activeMenu , setActiveMenu] = useState('/');
+    
+    useEffect(()=>{
+        const userLocation = pathName;
+        setActiveMenu(userLocation);
+        window.scroll(0 , 0)
+    },[pathName])
   return (
     <section className="block w-full py-8 bg-background-100 px-2">
         <div className="container">
             <div className="flex items-center justify-between">
                 <ul className="hidden lg:flex items-center gap-6">
                     <li className="block">
-                        <Link href="/" className='link'>خانه</Link>
+                        <Link href="/" className={activeMenu === '/' ? "link_hover" : "link"}>خانه</Link>
                     </li>
                     <li className="block">
-                        <Link href="/AboutMe" className='link'>درباه من</Link>
+                        <Link href="/AboutMe" className={activeMenu === '/AboutMe' ? "link_hover" : "link"}>درباه من</Link>
                     </li>
                     <li className="block">
-                        <Link href="/Projects" className='link'>پروژه ها</Link>
+                        <Link href="/Projects" className={activeMenu === '/Projects' ? "link_hover" : "link"}>پروژه ها</Link>
                     </li>
                     <li className="block">
-                        <Link href="/Articles" className='link'>بلاگ</Link>
+                        <Link href="/Articles" className={activeMenu === '/Articles' ? "link_hover" : "link"}>بلاگ</Link>
                     </li>
                 </ul>
                  <Link href="/" className='block uppercase font-iranBlack text-sm lg:text-3xl sm:text-2xl xs:text-sm'><span className="text-secondary-100">dalvnadi</span> - <span className="text-primary-100">front-end</span></Link>
 
-                 <Link href="/ContactMe" className='link'>تماس با من</Link>
+                 <Link href="/ContactMe" className={activeMenu === '/ContactMe' ? "link_hover" : "link"}>تماس با من</Link>
             </div>
         </div>
     </section>
